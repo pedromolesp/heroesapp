d0ef7059f8bbbb9969a12d100eff21d446d45156
Pedro Molina <pedro.molina@petroprix.com>
2021-11-10 16:51:16 +0100

    terminada clase 80 buscador a medias

--- src/routers/DashboardRoute.js
+++ src/routers/DashboardRoute.js
@@ -5,6 +5,7 @@ import { Switch, Route, Redirect } from 'react-router-dom'
 import { MarvelScreen } from '../components/marvel/MarvelScreen'
 import { HeroScreen } from '../components/heroes/HeroScreen'
 import { DcScreen } from '../components/dc/DcScreen'
+import { SearchScreen } from '../components/search/SearchScreen'
 
 export const DashboardRoute = () => {
     return (
@@ -12,6 +13,7 @@ export const DashboardRoute = () => {
             <NavBar />
             <div className="container mt-2">
                 <Switch>
+                    <Route exact path="/search" component={SearchScreen} />
                     <Route exact path="/marvel" component={MarvelScreen} />
                     <Route exact path="/hero/:heroId" component={HeroScreen} />
                     <Route exact path="/dc" component={DcScreen} />
